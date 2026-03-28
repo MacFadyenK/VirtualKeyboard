2d6793e023e55120cc805f1f43740f745b1dddff
MacFadyenK <khmacfadyen@oakland.edu>
2026-03-28 18:32:32 -0400

    ordered character selection

--- pipeline_functions/Characterselection.py
+++ pipeline_functions/Characterselection.py
@@ -10,7 +10,7 @@ letters = [
         ["5","6","7","8","9","_"]] 
     #The 6 x 6 grid of all character options. To select for only 8 characters, it would be best to reduce the matrix size (ex: 3 x 3).
 
-def create_flash_matrix(tensor): #(time, sample, num_outputs)
+def create_flash_matrix(tensor, y): #(time, sample, num_outputs)
 
     flash_matrix = np.zeros((6, 6)) #Initialize a 6x6 matrix of zeros to represent the flash pattern.
     
@@ -21,16 +21,16 @@ def create_flash_matrix(tensor): #(time, sample, num_outputs)
     #Adjusted verison without uses the nested for loops. Can go back if needed. 
     hits_per_flash = tensor[:, :, 1].sum(axis=1) #Extract the hit counts for each flash from the tensor for the second column P300.(samples)
     for index in range(len(hits_per_flash)):
-        if (index % 12) < 6:  # row flashes
-            flash_matrix[(index % 12), :] += hits_per_flash[index]
+        if y[index] <= 6:  # row flashes
+            flash_matrix[y[index] - 1, :] += hits_per_flash[index]
         else:          # column flashes
-            flash_matrix[:, (index % 12) - 6] += hits_per_flash[index]
+            flash_matrix[:, y[index] - 1 - 6] += hits_per_flash[index]
     return flash_matrix
 
 #P300 speller cycle character selection function
-def p300_speller_cycle(tensor): 
+def p300_speller_cycle(tensor, y): 
 
-    flash_matrix = create_flash_matrix(tensor)
+    flash_matrix = create_flash_matrix(tensor, y)
 
     # Sum across repetitions
     row_totals = np.sum(flash_matrix, axis=1) #Set to axis=1 to sum across rows, giving a total score for each row. This will help identify which row has the most hits.
@@ -44,12 +44,13 @@ def p300_speller_cycle(tensor):
 
     return predicted_letter, row_idx, col_idx, row_totals, col_totals
 
-# tensor = []
+tensor = []
+y= 0
 
-# #Printing the letter with the highest score in the row and column.
-# predicted_letter, row_idx, col_idx, row_scores, col_scores = p300_speller_cycle(tensor) 
+#Printing the letter with the highest score in the row and column.
+predicted_letter, row_idx, col_idx, row_scores, col_scores = p300_speller_cycle(tensor, y) 
 
-# print(f"Row scores: {row_scores}")
-# print(f"Column scores: {col_scores}")
-# print(f"Selected Row: {row_idx}, Selected Column: {col_idx}")
-# print(f"Predicted letter: {predicted_letter}")
+print(f"Row scores: {row_scores}")
+print(f"Column scores: {col_scores}")
+print(f"Selected Row: {row_idx}, Selected Column: {col_idx}")
+print(f"Predicted letter: {predicted_letter}")
